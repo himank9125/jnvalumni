999f2c414ad157147fc133a37c8f9202aa02165c
hamish <65891506+himank9125@users.noreply.github.com>
2024-11-17 10:46:31 +0000

    hh

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ const studentRouts = require("./Routs/student");
 const dotenv = require("dotenv");
 const ENV = process.env.NODE_ENV || "beta";
 dotenv.config({ path: `.env.${ENV}` });
+const cors = require("cors");  
 const app = express();
 
 app.use(express.json({ limit: "10mb" }));
